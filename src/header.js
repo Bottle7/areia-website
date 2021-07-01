a5e9a10905af4c05d36869b63b6afee7e5b332b4
Brandon <Brandonjbouchard@gmail.com>
2021-06-30 17:30:06 -0700

    First commit for beginning Areia'a website.

---
+++ src/header.js
@@ -0,0 +1,10 @@
+function Header() {
+    return (
+        <header>
+            <h1>Areia</h1>
+            <p>peripherals for your life</p>
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/main.js
@@ -0,0 +1,13 @@
+import './main.css';
+
+function Main() {
+    return (
+        <section>
+            <div className="main">
+                <p>Test</p>
+            </div>
+        </section>
+    );
+}
+
+export default Main;
